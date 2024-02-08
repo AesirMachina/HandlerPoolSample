6700d56cc721affa34949a56f0fe9a1d9a01198c
Richard Schilling <richard@aesirmachina.com>
2024-02-08 14:02:13 -0800

    Import AAR directly.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation("com.aesirmachina:handlerpool:0.1")
+    implementation(files("libs/handlerPoolLib-release-0.1.aar"))
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
